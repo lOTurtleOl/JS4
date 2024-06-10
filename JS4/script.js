96d8e0615ecef529b75f261d3afaf3220aee41e3
Andrew Hughes <andrewhughes@Andrews-Laptop.local>
2024-06-10 16:35:36 -0500

    question 3-5 completed

---
+++ JS4/script.js
@@ -0,0 +1,12 @@
+// Question 3
+console.log("You would call array.length - 1");
+
+// Question 4
+console.log('You would call index 0');
+
+// Question 5
+let nameLengths = [];
+for (let i = 0; i < names.length; i++) {
+    nameLengths.push(names[i].length) ;
+}
+console.log(nameLengths);
