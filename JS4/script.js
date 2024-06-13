4dee08292f6626d8e7fd66a8592d1af5264100fb
Andrew Hughes <andyashmore1@gmail.com>
2024-06-12 19:19:49 -0500

    add answers to question 1 and 2

--- JS4/script.js
+++ JS4/script.js
@@ -1,3 +1,25 @@
+// Question 1 - a
+let ages = [3, 9, 23, 64, 2, 8, 28, 93];
+ages[ages.length - 1] - ages[0];
+console.log(ages);
+// b
+ages.push(34);
+console.log(ages);
+// c
+sum = 0;
+for (let i = 0; i < ages.length; i++) {
+    sum += ages[i];
+}
+avg = sum / ages.length;
+console.log(avg)
+
+// Question 2 - a
+let names = ['Sam', 'Tommy', 'Tim', 'Sally', 'Buck', 'Bob']
+names.forEach(element => {
+    console.log(element);
+});
+
+
 // Question 3
 console.log("You would call array.length - 1");
 
